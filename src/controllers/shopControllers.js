031e517c7413cdeed096dc436b74c127d826ecd8
danyerng <danyerng@gmail.com>
2023-12-07 18:52:48 -0300

    [FEAT] Cargando el shop desde la BBDD falta la parte de css para mostrarlo de la manera correcta

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -1,6 +1,19 @@
+const {getAllProducts}  = require('../services/productServices');
+
 const shopControllers ={
-   // inicio: (req,res) => res.render('index'),
-    shop: (req,res) => res.render('pages/shop/shop'),
+   
+    //shop: (req,res) => res.render('pages/shop/shop'),
+    shop: async(req,res) => {
+        const data = await getAllProducts();
+        console.log( ' Datos del shopController ' , data);
+        res.render('pages/shop/shop' , 
+        {
+            view:{
+                title: 'Lista de productos en el shop '
+            },
+            items: data
+        });
+    },
     item: (req,res) => res.render('pages/shop/item'),
     itemId : (req,res) => res.render('pages/shop/item'),
     itemIdAdd : (req,res) => res.render('pages/shop/item'),
