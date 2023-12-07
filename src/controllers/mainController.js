af1ab5c5ee71a4caaa6557fa43f4ab3827cddda0
danyerng <danyerng@gmail.com>
2023-12-07 19:54:12 -0300

    [FEAT] Cargando el carrousel del index desde la BBDD falta arreglar los links a cada uno de los items especificos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,7 @@
 const {getAllItems} = require('../models/productModel');
 const {getProductById}  = require('../services/productServices');
 const {getItem} = require('../models/productModel');
+const {getAllProducts}  = require('../services/productServices');
 
 const mainControllers ={
     /*inicio: async (req,res) =>{
@@ -20,7 +21,18 @@ const mainControllers ={
         });
        // res.render('index');
     },*/
-    inicio:(req,res) => res.render('index'),
+    //inicio:(req,res) => res.render('index'),
+    inicio: async(req,res) => {
+        const data = await getAllProducts();
+        console.log( ' Datos del mainController ' , data);
+        res.render('index' , 
+        {
+            view:{
+                title: 'Lista de productos en el index '
+            },
+            items: data
+        });
+    },
     home: (req,res) => res.send('Route for Home view from controller'),
     contact:  (req,res) => res.render('pages/admin/contact'),
     about: (req,res) => res.send('Route for About view from controller'),
