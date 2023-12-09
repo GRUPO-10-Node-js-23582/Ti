97d7f56f63f797efa1f9426eac8d95b19afe9436
danyerng <danyerng@gmail.com>
2023-12-09 16:49:50 -0300

    [FEAT] Agregando las funciones de create edit y delete en el ProductModel y en el productService

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -32,7 +32,76 @@ const getItem = async (params) => {
     }
 }
 
+const create = async (params) => {
+    try{
+        const [rows] = await conn.query('INSERT INTO product (product_name, product_descripcion, price, stock, discount, sku, dues, image_front, image_back, licence_id, category_id) VALUES ?;',[params]);
+
+        const result = {
+            isError: false,
+            message: ' Se modifico el producto correctamente',
+            datos: rows
+        };
+        return result;
+    } catch (e){
+        const error = {
+            isError: true,
+            message: ` NO se pudo crear el producto con los valores aportados, error: ${e}`
+        };
+        return error;
+    } finally{
+        await conn.releaseConnection();
+    }
+};
+
+
+const edit = async (params,id) => {
+    try {
+        const [rows] = await conn.query(' UPDATE product SET ? WHERE product_id = ?',[params, id]);
+        const result = {
+            isError: false,
+            message: ' Se modifico el producto correctamente',
+            datos: rows
+        };
+        return result;
+    } catch (e){
+        const error = {
+            isError: true,
+            message: ` NO se pudo modificar el producto indicado, error: ${e}`
+        };
+        return error;
+    } finally{
+        await conn.releaseConnection();
+    }
+};
+
+
+const deleteProduct = async (id) => {
+    try {
+        const [rows] = await conn.query('DELETE FROM product WHERE ?;', id);
+        const result = {
+            isError: false,
+            message: ' Se elimino el producto correctamente',
+            datos: rows
+        };
+        return result;
+    } catch (e){
+        const error = {
+            isError: true,
+            message: ` NO se pudo eliminar el producto indicado, error: ${e}`
+        };
+        return error;
+    } finally{
+        await conn.releaseConnection();
+    }
+};
+
+
+
+
 module.exports = {
     getAllItems,
-    getItem
+    getItem,
+    create,
+    edit,
+    deleteProduct
 }
--- src/services/productServices.js
+++ src/services/productServices.js
@@ -8,7 +8,50 @@ const getProductById = async(id) => {
     return await productModel.getItem(id);
 }
 
+const createProduct = async (item) =>{
+    const productSchema={
+        product_name: item.name,
+        product_description: item.description,
+        price: item.price,
+        stock: item.stock,
+        discount: item.discount,
+        sku: item.sku,
+        dues: item.dues,
+        image_front: item.image_front,
+        image_back: item.image_back,
+        licence_id: item.licence_id,
+        category_id: item.category_id
+    }
+    return await productModel.create([Object.values(productSchema)]);   
+} 
+
+const editProduct = async (item,id) =>{
+    const productSchema={
+        product_name: item.name,
+        product_description: item.description,
+        price: item.price,
+        stock: item.stock,
+        discount: item.discount,
+        sku: item.sku,
+        dues: item.dues,
+        image_front: item.image_front,
+        image_back: item.image_back,
+        licence_id: item.licence_id,
+        category_id: item.category_id
+    }
+    return await productModel.edit(productSchema, id );   
+} 
+
+const deleteProduct = async(id) => {
+    return await productModel.delete(id);
+}
+
+
+
 module.exports={
     getProductById,
-    getAllProducts
+    getAllProducts,
+    createProduct,
+    editProduct,
+    deleteProduct
 }
