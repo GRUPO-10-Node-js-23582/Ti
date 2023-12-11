75b00bc3f812cf97daed73cd4edaa505c56f4f0c
danyerng <danyerng@gmail.com>
2023-12-10 21:14:45 -0300

    [FEAT] Se carga un nuevo create.ejs para ver porque no funciona el req.body

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+//const methodOverride = require('method-override');
 const mainRoutes = require('./src/routes/mainRoutes');
 const shopRoutes = require('./src/routes/shopRoutes');
 const adminRoutes = require('./src/routes/adminRoutes');
@@ -13,10 +14,15 @@ app.set('views', path.join(__dirname, './src/views'));
 //app.use (express.static('public'));
 app.use(express.static(path.resolve(__dirname,"public")));
 
+//app.use(methodOverride('_method')); // para usar PUT y DELETE
+
 app.use('/', mainRoutes);
 app.use('/', shopRoutes);
 app.use('/admin', adminRoutes);
 app.use('/', authRoutes);
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/ping', (req,res) => res.send('pong'));
 
--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,5 +1,7 @@
 //const {getAllItems} = require('../models/productModel');
 const {getAllProducts}  = require('../services/productServices');
+const {createProduct}  = require('../services/productServices');
+//const { item } = require('./shopControllers');
 
 const adminControllers ={
    /* admin: async (req,res) =>{
@@ -28,12 +30,22 @@ const adminControllers ={
             items: data
         });
     },
-    getCreate: (req,res) => res.render('pages/admin/create'),
-    postCreate : (req,res) => res.render('pages/admin/create'),
+    getCreate:  (req,res) => res.render('pages/admin/create'),
+
+    postCreate : async (req,res) => {
+        console.log(" Sku desde el body ", req.body.sku);
+        const item = req.body;
+        console.log(" Item desde el body ", item);
+        
+        const product = await createProduct(item);
+        console.log(" Datos desde el adminCOntroller ", item);
+
+        res.redirect('pages/admin/admin');
+    },
     postEdit : (req,res) => res.render('pages/admin/edit'),
     putEdit :(req,res) => res.render('pages/admin/edit'),
     delete:(req,res) => res.render('pages/admin/admin'),
-  
+
  }
  
  module.exports = adminControllers;
