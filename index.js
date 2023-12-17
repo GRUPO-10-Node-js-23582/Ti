0b8a632d116325b4edff7d6d62f5dca448a368d7
danyerng <danyerng@gmail.com>
2023-12-17 14:37:03 -0300

    [FEAT] Se cambia resave: true, para intentar que no se borre el loggeo en el server

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, './src/views'));
 
 app.use(session({
     secret: "S3cr3t10H@sh",
-    resave: false,
+    resave: true,
     saveUninitialized: true,
 }));
 //app.use (express.static('public'));
